59dd59200f076824916844a4edecf480f64640a6
Derrick <derek.aungkhant@protonmail.com>
2019-09-15 13:44:44 +0630

    New lessons

--- Lesson 4/loops.js
+++ Lesson 4/loops.js
@@ -1,2 +1,42 @@
 // 1) if conditional
 
+/*
+== checks if values are equal but does not check type
+=== also checks if the datatypes are the same
+*/
+
+const x = 10;
+
+// check if x is equal to 10
+if (x=='10'){
+    console.log('x is 10');
+}else{
+    console.log('x is not 10')
+}
+
+if (x !== '10'){
+    console.log('x is not equal to 10 in terms of datatype')
+}
+
+// 2) or, and
+
+delicious = true;
+cheap = false;
+
+if (delicious || cheap){
+    console.log('buy that')
+}
+
+if (delicious && cheap){
+    console.log('worth it')
+}
+
+// imagine a scenario where the playground allows only kids above age 10
+const peter = 11
+const john = 9
+let is_allowed_peter = peter>10 ? true:false;
+let is_allowed_john = john>10 ? true:false;
+console.log("is peter allowed? "+is_allowed_peter)
+console.log("is john allowed? "+is_allowed_john)
+
+
